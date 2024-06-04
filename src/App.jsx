c242c7ac879c96faedbcb333eb66a81d92f12768
GBOnline50 <suportesinop50@gbonline.com.br>
2024-06-04 14:07:11 -0400

    mudancas

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,14 @@ import './App.css'
 const Titulo = Title
 const Cabecalho = Header
 
+function Form() {
+  const [to, setTo] = useState('')
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+}
+
 export default function App() {
   return (
     <>
@@ -12,14 +20,15 @@ export default function App() {
     <label >Completed</label>
     <input type="checkbox" id="completed" value="completed" />
     <label for="Priority">Priority</label>
-    <select id="cars" name="cars">
+    <select id="prioridade" name="Prioriry">
       <option value=""></option>
       <option value="Low">Low</option>
       <option value="Medium">Medium</option>
       <option value="High">High</option>
     </select>
-    <label for="fname">Title:</label>
+
     <input type="text" id="fname" name="fname"/>
+    <button id="Filter">Filter</button>
     </Cabecalho>
     <TodoList />
     </>
